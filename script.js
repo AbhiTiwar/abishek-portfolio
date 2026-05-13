bd3c6dd585023172e27b3e8f6e1fc6428f396fa5
Abishek Tiwari <71210506+AbhiTiwar@users.noreply.github.com>
2026-05-13 15:31:10 +0545

    Update script.js

--- script.js
+++ script.js
@@ -34,13 +34,22 @@ function type() {
 type();
 
 // Theme toggle (simple)
-function toggleTheme() {
-    document.body.classList.toggle("light");
+// Load saved theme
+window.onload = () => {
+    const savedTheme = localStorage.getItem("theme");
 
-    // optional: save preference
-    if (document.body.classList.contains("light")) {
-        localStorage.setItem("theme", "light");
-    } else {
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark");
+    }
+};
+
+// Toggle theme
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
         localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
     }
 }
