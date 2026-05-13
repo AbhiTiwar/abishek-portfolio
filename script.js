55f775b680288bc0bd2952b187283fdd70b24e30
Abishek Tiwari <71210506+AbhiTiwar@users.noreply.github.com>
2026-05-13 15:15:18 +0545

    Update script.js

--- script.js
+++ script.js
@@ -36,4 +36,11 @@ type();
 // Theme toggle (simple)
 function toggleTheme() {
     document.body.classList.toggle("light");
+
+    // optional: save preference
+    if (document.body.classList.contains("light")) {
+        localStorage.setItem("theme", "light");
+    } else {
+        localStorage.setItem("theme", "dark");
+    }
 }
