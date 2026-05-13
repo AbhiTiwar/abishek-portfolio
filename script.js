e7688516db6cb4fb3c8c76f4b02d3662d27d5884
Abishek Tiwari <71210506+AbhiTiwar@users.noreply.github.com>
2026-05-13 16:03:09 +0545

    Update script.js

--- script.js
+++ script.js
@@ -135,3 +135,21 @@ window.addEventListener("scroll",()=>{
 function toggleTheme(){
     document.body.classList.toggle("light");
 }
+
+
+
+// LOAD SAVED THEME
+if(localStorage.getItem("theme") === "light"){
+    document.body.classList.add("light");
+}
+
+function toggleTheme(){
+
+    document.body.classList.toggle("light");
+
+    if(document.body.classList.contains("light")){
+        localStorage.setItem("theme","light");
+    } else {
+        localStorage.setItem("theme","dark");
+    }
+}
